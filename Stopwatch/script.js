64e497ca0250bf994cde62d82545dc65995af160
PeterVysochina <petya.viso4ina@gmail.com>
2021-07-02 11:46:16 +0300

    OOP Stopwatch

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -1,114 +1,174 @@
-const watch = document.querySelector('.stopwatch__displaytime');
-const subWatch = document.querySelector('.stopwatch__laptime');
-const laplist = document.querySelector(".laps__list");
-const totalTime = document.querySelector(".total__time");
-const lapsTitle = document.querySelector(".laps__title");
-const totalTitle = document.querySelector(".total__title");
-const lapTitle = document.querySelector(".lapCounter__title");
-const lap = document.querySelector(".lap__list");
-
-const startButton = document.querySelector(".start");
-const stopButton = document.querySelector(".stop");
-const contButton = document.querySelector(".cont");
-const resetButton = document.querySelector(".reset");
-const lapButton = document.querySelector(".lap");
-
-let milliseconds = 0;
-let timer;
-let subMiliseconds = 0;
-let subTimer;
-let lapCounter = 0;
-
-function converter(data) {
-	return	('0'+data.getUTCHours()).slice(-2) + ':' +
-			('0'+data.getUTCMinutes()).slice(-2) + ':' +
-			('0'+data.getUTCSeconds()).slice(-2) + ':' +
-			('0'+data.getUTCMilliseconds()/10).slice(-2);
-}
-
-function start() {
-	clearInterval(timer);
-	timer = setInterval( () => {
-		milliseconds += 10;
-		let timer = new Date(milliseconds);
-		watch.textContent = converter(timer);
-	},10);
-
-	startButton.classList.add('invisible');
-	stopButton.classList.remove('invisible');
-	lapButton.classList.remove('invisible');
-};
-
-function startLap() {
-    clearInterval(subTimer);
-	subTimer = setInterval( () => {
-		subMiliseconds += 10;
-		let subTimer = new Date(subMiliseconds);
-		subWatch.textContent = converter(subTimer);
-	},10);
-}
-
-function pause() {
-    clearInterval(timer);
-    clearInterval(subTimer);
-
-	stopButton.classList.add('invisible');
-	lapButton.classList.add('invisible');
-	contButton.classList.remove('invisible');
-	resetButton.classList.remove('invisible');
-};
-
-function cont() {
-    start()
-    if (subWatch.textContent) {   
-        startLap();
+class StopwatchLogic {
+    constructor() {
+        this.milliseconds = 0;
+        this.subMiliseconds = 0;
+        this.lapCount = 0;
+        this.subTimer;
+        this.timer;
     }
 
-	contButton.classList.add('invisible');
-	resetButton.classList.add('invisible');
-	stopButton.classList.remove('invisible');
-	lapButton.classList.remove('invisible');
-}
-
-function reset() {
-	    clearInterval(timer);
-        clearInterval(subTimer);
-	    milliseconds = 0;
-        subMiliseconds = 0;
-	    watch.textContent = '00:00:00:00';
-        subWatch.textContent = ''; 
-        laplist.innerHTML = '';
-        totalTime.innerHTML = '';
-		lap.innerHTML = '';
-		lapCounter = 0;
-
-		startButton.classList.remove('invisible');
-		contButton.classList.add('invisible');
-		resetButton.classList.add('invisible');
-		lapsTitle.classList.add('invisible');
-		totalTitle.classList.add('invisible');
-		lapTitle.classList.add('invisible');
-};
-
-function addLap() {
-	subMiliseconds = 0;
-	startLap()
-    if(!laplist.innerHTML) {
-        laplist.innerHTML = laplist.innerHTML +`<div class="newLap">${watch.textContent}</div>`;
-    } else {
-        laplist.innerHTML = laplist.innerHTML +`<div class="newLap">${subWatch.textContent}</div>`;
+    start() {
+        clearInterval(this.timer);
+        this.timer = setInterval( () => {
+            this.milliseconds += 10;
+            let timer = new Date(this.milliseconds);
+            this.watch.textContent =
+                ('0'+timer.getUTCHours()).slice(-2) + ':' +
+                ('0'+timer.getUTCMinutes()).slice(-2) + ':' +
+                ('0'+timer.getUTCSeconds()).slice(-2) + ':' +
+                ('0'+timer.getUTCMilliseconds()/10).slice(-2);
+        },10);
+    
+        this.startButton.classList.add('invisible');
+        this.stopButton.classList.remove('invisible');
+        this.lapButton.classList.remove('invisible');
+    };
+    
+    startLap() {
+        clearInterval(this.subTimer);
+        this.subTimer = setInterval( () => {
+            this.subMiliseconds += 10;
+            let subTimer = new Date(this.subMiliseconds);
+            this.subWatch.textContent =
+                ('0'+subTimer.getUTCHours()).slice(-2) + ':' +
+                ('0'+subTimer.getUTCMinutes()).slice(-2) + ':' +
+                ('0'+subTimer.getUTCSeconds()).slice(-2) + ':' +
+                ('0'+subTimer.getUTCMilliseconds()/10).slice(-2);
+        },10);
     }
-	lap.innerHTML = lap.innerHTML + `<div class="newLap">${lapCounter += 1}</div>`
-    totalTime.innerHTML = totalTime.innerHTML + `<div class="newLap">${watch.textContent}</div>`;
-
-	lapsTitle.classList.remove('invisible')
-	totalTitle.classList.remove('invisible')
-	lapTitle.classList.remove('invisible')
+    
+    pause() {
+        clearInterval(this.timer);
+        clearInterval(this.subTimer);
+    
+        this.stopButton.classList.add('invisible');
+        this.lapButton.classList.add('invisible');
+        this.contButton.classList.remove('invisible');
+        this.resetButton.classList.remove('invisible');
+    };
+    
+    cont() {
+        this.start()
+        if (this.subWatch.textContent) {   
+            this.startLap();
+        }
+    
+        this.contButton.classList.add('invisible');
+        this.resetButton.classList.add('invisible');
+        this.stopButton.classList.remove('invisible');
+        this.lapButton.classList.remove('invisible');
+    }
+    
+    reset() {
+            clearInterval(this.timer);
+            clearInterval(this.subTimer);
+            this.milliseconds = 0;
+            this.subMiliseconds = 0;
+            this.watch.textContent = '00:00:00:00';
+            this.subWatch.textContent = ''; 
+            this.lapList.innerHTML = '';
+            this.totalTime.innerHTML = '';
+            this.lapsList.innerHTML = '';
+            this.lapCount = 0;
+    
+            this.startButton.classList.remove('invisible');
+            this.contButton.classList.add('invisible');
+            this.resetButton.classList.add('invisible');
+            this.lapsTitle.classList.add('invisible');
+            this.totalTitle.classList.add('invisible');
+            this.lapTitle.classList.add('invisible');
+    };
+    
+    addLap() {
+        this.subMiliseconds = 0;
+        this.startLap()
+        if(!this.lapsList.innerHTML) {
+            this.lapsList.innerHTML = this.lapsList.innerHTML +`<div class="newLap">${this.watch.textContent}</div>`;
+        } else {
+            this.lapsList.innerHTML = this.lapsList.innerHTML +`<div class="newLap">${this.subWatch.textContent}</div>`;
+        }
+        this.lapList.innerHTML = this.lapList.innerHTML + `<div class="newLap">${this.lapCount += 1}</div>`
+        this.totalTime.innerHTML = this.totalTime.innerHTML + `<div class="newLap">${this.watch.textContent}</div>`;
+    
+        this.lapsTitle.classList.remove('invisible')
+        this.totalTitle.classList.remove('invisible')
+        this.lapTitle.classList.remove('invisible')
+    }
 }
 
+class Stopwatch extends StopwatchLogic {
+    constructor() {
+        super()
+        this.stopwatches = document.querySelector('.stopwatches');
+        this.stopwatch = document.createElement('div');
+        this.watch = document.createElement('h2');
+        this.subWatch = document.createElement('h3');
+        this.stopwatchInfo = document.createElement('div');
+        this.lapCounter = document.createElement('div');
+        this.laps = document.createElement('div');
+        this.total = document.createElement('div');
+        this.lapTitle = document.createElement("h4");
+        this.lapsTitle = document.createElement("h4");
+        this.totalTitle = document.createElement("h4");
+        this.lapList = document.createElement("div");
+        this.lapsList = document.createElement("div");
+        this.totalTime = document.createElement("div");
+        this.stopwatchButtons = document.createElement('div')
+        this.startButton = document.createElement("button");
+        this.stopButton = document.createElement("button");
+        this.contButton = document.createElement("button");
+        this.resetButton = document.createElement("button");
+        this.lapButton = document.createElement("button");
+    }
+    render() {
+        this.stopwatch.classList.add('stopwatch')
+        this.watch.classList.add('stopwatch__displaytime')
+        this.subWatch.classList.add('stopwatch__laptime')
+        this.stopwatchInfo.classList.add('stopwatch__info')
+        this.lapCounter.classList.add('lapCounter')
+        this.laps.classList.add('laps')
+        this.total.classList.add('total')
+        this.lapTitle.classList.add('lapCounter__title', 'invisible')
+        this.lapsTitle.classList.add('laps__title', 'invisible')
+        this.totalTitle.classList.add('total__title', 'invisible')
+        this.lapList.classList.add('lap__list')
+        this.lapsList.classList.add('laps__list')
+        this.totalTime.classList.add('total__time')
 
-document.querySelector('.start').addEventListener('click', () => start());
-document.querySelector('.stop').addEventListener('click', () => pause());
-document.querySelector('.reset').addEventListener('click', () => reset());
-document.querySelector('.cont').addEventListener('click', () => cont());
-document.querySelector(".lap").addEventListener("click", () => addLap())
+        this.stopwatchButtons.classList.add('stopwatch__buttons')
+        this.startButton.classList.add('start', 'button');
+        this.stopButton.classList.add('stop', 'button', 'invisible');
+        this.contButton.classList.add('cont', 'button', 'invisible');
+        this.resetButton.classList.add('reset', 'button', 'invisible');
+        this.lapButton.classList.add('lap', 'button', 'invisible');
+
+        this.watch.textContent = '00:00:00:00';
+        this.lapTitle.textContent = 'Lap';
+        this.lapsTitle.textContent = 'Lap Time';
+        this.totalTitle.textContent = 'Total Time';
+
+        this.startButton.textContent = 'start';
+        this.stopButton.textContent = 'stop';
+        this.contButton.textContent = 'continue';
+        this.resetButton.textContent = 'reset';
+        this.lapButton.textContent = 'lap';
+
+        this.lapCounter.append(this.lapTitle, this.lapList);
+        this.laps.append(this.lapsTitle, this.lapsList);
+        this.total.append(this.totalTitle, this.totalTime);
+        this.stopwatchInfo.append(this.lapCounter, this.laps, this.total);
+        this.stopwatchButtons.append(this.startButton, this.stopButton, this.contButton, this.resetButton, this.lapButton);
+        this.stopwatch.append(this.watch, this.subWatch, this.stopwatchInfo, this.stopwatchButtons);
+        this.stopwatches.append(this.stopwatch);
+
+        this.startButton.addEventListener('click', this.start.bind(this));
+        this.stopButton.addEventListener('click', this.pause.bind(this));
+        this.contButton.addEventListener('click', this.cont.bind(this));
+        this.resetButton.addEventListener('click', this.reset.bind(this));
+        this.lapButton.addEventListener("click", this.addLap.bind(this))
+    }
+}
+const addButton = document.querySelector('.addSW')
+addButton.addEventListener('click', function() {
+   new Stopwatch().render()
+})
